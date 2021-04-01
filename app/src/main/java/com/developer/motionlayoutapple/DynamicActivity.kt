676c959d04165ca03beaca9471966594fcf51ffd
Allef Sousa <allefsousa_1@hotmail.com>
2021-04-01 14:20:10 -0300

    finish primary sample

--- app/src/main/java/com/developer/motionlayoutapple/DynamicActivity.kt
+++ app/src/main/java/com/developer/motionlayoutapple/DynamicActivity.kt
@@ -2,10 +2,26 @@ package com.developer.motionlayoutapple
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.motion.widget.MotionLayout
+import kotlinx.coroutines.*
 
 class DynamicActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+//        GlobalScope.launch (Dispatchers.IO){
+//            delay(1000)
+//            withContext(Dispatchers.Main){
+//                val motion = findViewById<MotionLayout>(R.id.motion_root)
+//                motion.transitionToEnd()            }
+//        }
+
+
     }
 }
